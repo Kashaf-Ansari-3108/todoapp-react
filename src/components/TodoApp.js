86c35ea8e8f3b82ed8de3ca9db82e324dd66c9d6
Kashaf Ansari <kashafansari3108@gmail.com>
2022-09-17 00:09:34 +0500

    complete

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,67 +1,103 @@
-import {React,useState} from 'react'
+import { React, useState } from "react";
 
 const TodoApp = () => {
-    const [todos, setTodos] = useState([]); 
-    const [todo, setTodo] = useState('');
-    const [isEdit, setIsEdit] = useState(false);
-    const [indexVal, setindexVal] = useState();
-    const add = (e)=>{
-      if(todo == ''){
-       e.disabled = true
-      }
-      else{
-        if (isEdit) {
-          // editing 
-          todos[indexVal] = todo
-          setTodos([...todos])
-          setIsEdit(false)
-          setTodo('')
-        } else {
-          //add
-          setTodos([...todos, todo])
-          setTodo('')
-        }
-      }
-      
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+  const [indexNumber, setIndexNumber] = useState(null);
+  const [updateInput, setupdateInput] = useState("");
+
+  const add = (e) => {
+    if (todo == "") {
+      e.disabled = true;
+    } else {
+      setTodos([...todos, todo]);
+      setTodo("");
     }
-    const delAll = ()=>{
-        setTodos([]);
-    } 
-    const delTodo = (i)=>{
-      todos.splice(i, 1)
-      setTodos([...todos])
+  };
+  const delAll = () => {
+    setTodos([]);
+  };
+  const delTodo = (i) => {
+    todos.splice(i, 1);
+    setTodos([...todos]);
+  };
+  const updateTodo = (i, e) => {
+    if (updateInput == "") {
+      e.disabled = true;
+    } else {
+      todos.splice(i, 1, updateInput);
+      setTodos([...todos]);
+      setIndexNumber(null);
+      setupdateInput("");
     }
-    const editTodo = (i) => {
-      setTodo(todos[i])
-      setIsEdit(true)
-      setindexVal(i)
-    }
-    
+  };
+  const editTodo = (i) => {
+    setupdateInput(todos[i]);
+  };
+
   return (
     <>
-     <div id="todos">
+      <div id="todos">
         <div className="heading">
-            <h1>Add Todo List</h1>
+          <h1>Add Todo List</h1>
         </div>
         <div className="todo-input-container">
-            <input id="todoItem" value={todo} onChange={(e)=>setTodo(e.target.value)} className="todo_input" type="text" placeholder="Add Task..."/>
-            <i className="fas fa-plus add-item" onClick={(e)=>add(e)}></i>
-            <i className="fas fa-trash-alt add-item deleteBtn" onClick={delAll}></i>
-        </div>  
-    </div>
-    <div className="todo-list-container">
-    {todos.map((todo,i)=>(
- <ul key={i} id="todoList">
-        <li className='todoList'>{todo}
-          <i className='fas fa-edit editBtn' onClick={()=>editTodo(i)}></i>
-          <i className='fas fa-trash-alt  delBtn' onClick={()=>delTodo(i)}></i>
-        </li>
-      </ul>
-   ))} 
-</div>
-   
-   </>
-  )
-}
+          <input
+            id="todoItem"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+            className="todo_input"
+            type="text"
+            placeholder="Add Task..."
+          />
+          <i className="fas fa-plus add-item" onClick={(e) => add(e)}></i>
+          <i
+            className="fas fa-trash-alt add-item deleteBtn"
+            onClick={delAll}
+          ></i>
+        </div>
+      </div>
+      <div className="todo-list-container">
+        {todos.map((todo, i) => (
+          <ul key={i} id="todoList">
+            {indexNumber == i ? (
+              <>
+                <div className="updateInput">
+                  <input
+                    autoFocus
+                    className="update"
+                    value={updateInput}
+                    onChange={(e) => setupdateInput(e.target.value)}
+                    type="text"
+                  />
+                  <i
+                    onClick={(e) => updateTodo(i, e)}
+                    className="fas fa-plus add-item updateBtn"
+                    aria-hidden="true"
+                  ></i>
+                </div>
+              </>
+            ) : (
+              <li className="todoList">
+                {todo}
+                <i
+                  className="fas fa-edit editBtn"
+                  onClick={() => {
+                    setIndexNumber(i);
+                    editTodo(i);
+                  }}
+                ></i>
+                <i
+                  className="fas fa-trash-alt  delBtn"
+                  onClick={() => delTodo(i)}
+                ></i>
+              </li>
+            )}
+          </ul>
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default TodoApp
+export default TodoApp;
